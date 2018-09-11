909697bafc6b9b95f23ab7c8f86be34651063136
mengfanwang <33670609+mengfanwang@users.noreply.github.com>
2018-09-11 15:08:16 -0400

    ha

---
+++ Stochastic Signal Systems/2/draft.m
@@ -0,0 +1,29 @@
+clc;clear;close all;
+% plot([-2 0],[0 0],'r');hold on;
+% plot([0 5],[0 0.25],'r');hold on;
+% plot([5 5],[0.25 0.5],'r');hold on;
+% plot([5 10],[0.5 0.75],'r');hold on;
+% plot([10 10],[0.75 1],'r');hold on;
+% plot([10 12],[1 1],'r');hold on;
+% title('Cumulative Distribution Function');
+
+% x = 0:0.01:10;
+% y = 0.5*exp(0.5*(3.5-x));
+% for ii = 1: length(y)
+%     if y(ii)>0.5
+%         y(ii) = 0;
+%     end
+% end
+% plot(x,y);
+% text(3.5,0.01,'t');
+
+format rat
+a = 6
+b = 6/4
+c = 6/9
+d = 6/16
+e = a+b+c+d
+d/e
+
+
+
